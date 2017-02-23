7a88585f4cb3f179f16f53889261c85425b83437
Joël Dumoulin <joel@dumoulin.ch>
2017-02-23 19:44:50 +0100

    Read configuration file

--- python_rocks/config.py
+++ python_rocks/config.py
@@ -1,16 +1,39 @@
+class Endpoint:
+    def __init__(self):
+        self.latency = 0 # Ld
+        self.nb_cache_servers = 0 # K
+        self.connections = {} # id: c, value: Lc
+
+class RequestDescription:
+    def __init__(self):
+        self.requested_video_id = 0 # Rv
+        self.endpoint_id = 0 # Re
+        self.nb_requests = 0 # Rn
+
 class Config:
     def __init__(self, filename):
         self.filename = filename
-        self.row = 0
-        self.col = 0
-        self.l = 0
-        self.h = 0
 
-        self.pizza = []
+        # First section
+        self.nb_videos = 0 # V
+        self.nb_endpoints = 0 # E
+        self.nb_request_descriptions = 0 # R
+        self.nb_caches = 0 # C
+        self.capacity = 0 # X
+
+        # 2nd section
+        self.video_sizes = []
+
+        # 3rd section
+        self.endpoints = []
+
+        # 4th section
+        self.request_descriptions = []
 
         self.valid = False
 
         self.read_configuration()
+
 
     def read_configuration(self):
         """
@@ -19,15 +42,45 @@ class Config:
         try:
             f = open(self.filename, 'r')
 
-            conf = f.readline().split(" ")
-            self.row = int(conf[0])
-            self.col = int(conf[1])
-            self.l = int(conf[2])
-            self.h = int(conf[3])
+            # First line : V, E, R, C, X
+            first_line = f.readline().split(" ")
+            self.nb_videos = int(first_line[0])
+            self.nb_endpoints = int(first_line[1])
+            self.nb_request_descriptions = int(first_line[2])
+            self.nb_caches = int(first_line[3])
+            self.capacity = int(first_line[4])
 
-            for r in range(self.row):
-                line = f.readline()
-                self.pizza.append(list(line))
+            # Second section : videos sizes
+            videos_sizes = f.readline().split(" ")
+            for video_size in videos_sizes:
+                self.video_sizes.append(int(video_size))
+
+            # Third section : endpoints
+            for i in range(self.nb_endpoints):
+                endpoint_conf = f.readline().split(" ")
+                endpoint = Endpoint()
+                endpoint.latency = int(endpoint_conf[0])
+                endpoint.nb_cache_servers = int(endpoint_conf[1])
+
+                for j in range(endpoint.nb_cache_servers):
+                    connection_description = f.readline().split(" ")
+                    c = int(connection_description[0])
+                    Lc = int(connection_description[1])
+                    endpoint.connections[c] = Lc
+
+                self.endpoints.append(endpoint)
+
+            # Last section : request descriptions
+            for i in range(self.nb_request_descriptions):
+                request_description = RequestDescription()
+                line = f.readline().split(" ")
+
+                request_description.requested_video_id = int(line[0])
+                request_description.endpoint_id = int(line[1])
+                request_description.nb_requests = int(line[2])
+
+                self.request_descriptions.append(request_description)
+
 
             self.valid = True
         except:
--- python_rocks/main.py
+++ python_rocks/main.py
@@ -9,22 +9,30 @@ from solver import Solver, SolverConfig
 INPUTS_PATH = "inputs"
 OUTPUTS_PATH = "outputs"
 
-config_filename = os.path.join(INPUTS_PATH, "small.in")
+INPUT_FILES = ["kittens", "me_at_the_zoo", "trending_today", "videos_worth_spreading"]
+
+EXAMPLE = 1
+SMALL = 3
+MEDIUM = 2
+BIG = 0
+
+CURRENT = EXAMPLE
 
 if __name__ == "__main__":
+    config_filename = os.path.join(INPUTS_PATH, INPUT_FILES[CURRENT] + ".in")
     config = Config(config_filename)
 
-    #if config.valid:
-    solvers = []
-    solver_configs = []
+    if config.valid:
+        solvers = []
+        solver_configs = []
 
-    solver_configs.append(SolverConfig("Test"))
+        solver_configs.append(SolverConfig("Test"))
 
-    for solver_config in solver_configs:
-        solvers.append(Solver(config, solver_config))
+        for solver_config in solver_configs:
+            solvers.append(Solver(config, solver_config))
 
-    for solver in solvers:
-        solver.solve()
+        for solver in solvers:
+            solver.solve()
 
-    best_solution = max(solvers, key=attrgetter('score'))
-    print("Best solver: {} ({})".format(best_solution.solver_config.name, best_solution.score))
+        best_solution = max(solvers, key=attrgetter('score'))
+        print("Best solver: {} ({})".format(best_solution.solver_config.name, best_solution.score))
