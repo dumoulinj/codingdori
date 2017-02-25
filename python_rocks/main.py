a1c7e4051139d6d3b7384e673428d350198e7087
Joël Dumoulin <joel@dumoulin.ch>
2017-02-25 17:42:56 +0100

    Easier to create batch

--- python_rocks/main.py
+++ python_rocks/main.py
@@ -1,38 +1,66 @@
 import os
 
 from operator import attrgetter
+import numpy as np
 
 
 from config import Config
+from solution import Solution
 from solver import Solver, SolverConfig
 
 INPUTS_PATH = "inputs"
 OUTPUTS_PATH = "outputs"
 
-INPUT_FILES = ["kittens", "me_at_the_zoo", "trending_today", "videos_worth_spreading"]
+CONF_NAMES = ["kittens", "me_at_the_zoo", "trending_today", "videos_worth_spreading"]
 
 EXAMPLE = 1
 SMALL = 3
 MEDIUM = 2
 BIG = 0
 
-CURRENT = EXAMPLE
+CONFS = [EXAMPLE, SMALL, MEDIUM, BIG]
 
 if __name__ == "__main__":
-    config_filename = os.path.join(INPUTS_PATH, INPUT_FILES[CURRENT] + ".in")
-    config = Config(config_filename)
+    for conf_id in CONFS:
+        print("Config : {}".format(CONF_NAMES[conf_id]))
+        config_filename = os.path.join(INPUTS_PATH, CONF_NAMES[conf_id] + ".in")
+        config = Config(config_filename)
 
-    if config.valid:
-        solvers = []
-        solver_configs = []
+        if config.valid:
+            solvers = []
+            solver_configs = []
 
-        solver_configs.append(SolverConfig("Test"))
+            alphas = [2, 5, 10, 15, 20, 25, 30]
+            betas = [0.1, 0.3, 0.5, 0.8]
+            gamas = [0.1, 0.3, 0.5, 0.8]
 
-        for solver_config in solver_configs:
-            solvers.append(Solver(config, solver_config))
+            _bgs = [(0.1, 0.8,),
+                  (0.2, 0.8,),
+                  (0.3, 0.8,),
+                  (0.8, 0.1,),
+                  (0.8, 0.2,),
+                  (0.8, 0.3,)]
 
-        for solver in solvers:
-            solver.solve()
+            for a in alphas:
+                for bg in _bgs:
+                    b = bg[0]
+                    g = bg[1]
+                    sconf = SolverConfig("Test alpha={}, beta={}, gamma={}".format(str(a), str(b), str(g)))
+                    sconf.alpha = a
+                    sconf.beta = b
+                    sconf.gama = g
+                    solver_configs.append(sconf)
 
-        best_solution = max(solvers, key=attrgetter('score'))
-        print("Best solver: {} ({})".format(best_solution.solver_config.name, best_solution.score))
+            for solver_config in solver_configs:
+                solvers.append(Solver(config, solver_config))
+
+            for solver in solvers:
+                solver.solve()
+
+            best_solution = max(solvers, key=attrgetter('score'))
+            print("Best solver: {} ({})".format(best_solution.solver_config.name, int(best_solution.score)))
+
+            sol = best_solution.best_solution
+            print()
+            out_filename = os.path.join(OUTPUTS_PATH, CONF_NAMES[conf_id] + ".out")
+            sol.write_result(out_filename)
